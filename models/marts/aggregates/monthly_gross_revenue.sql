7f3da08bceec624d78b5dfd805472ecd30a937b7
Steve Dowling <steve.dowling@fishtownanalytics.com>
2022-07-27 19:38:40 +0000

    project config

--- models/marts/aggregates/monthly_gross_revenue.sql
+++ models/marts/aggregates/monthly_gross_revenue.sql
@@ -1,8 +1,8 @@
 select
-    date_trunc(month, order_date) as order_month,
+    DATE_TRUNC('month', to_date(order_date, 'YYYY-MM-DD')) as order_month,
     sum(gross_item_sales_amount) as gross_revenue
 
-from {{ ref('fct_order_items') }}
+from "dev"."dbt_sdowling_core_analytics"."fct_order_items"
     group by 
         order_month
     order by 
